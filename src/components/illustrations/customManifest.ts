2067f3e5f9d9e09d3b6c951675f9739dd624d3e5
Michael Ufondu <michaelnufondu@gmail.com>
2026-07-01 10:11:53 -0700

    feat: add custom Nna ochie illustration

--- src/components/illustrations/customManifest.ts
+++ src/components/illustrations/customManifest.ts
@@ -2,6 +2,7 @@ import { ImageSourcePropType } from 'react-native';
 
 export const CUSTOM_ILLUSTRATIONS: Record<string, ImageSourcePropType> = {
   // family
+  'family:grandfather': require('../../../assets/illustrations/custom/family/grandfather.png'),
 
   // body
 
